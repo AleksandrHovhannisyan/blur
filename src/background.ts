a1d12954a30844464e265e13a7f7d35e8d9fff08
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2024-09-29 11:14:39 -0500

    Add rangeCount check

--- src/background.ts
+++ src/background.ts
@@ -12,7 +12,7 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
       target: { tabId: tab.id },
       func: function blurSelection() {
         const selection = window.getSelection();
-        if (!selection) return;
+        if (!selection || !selection.rangeCount) return;
         const span = document.createElement("span");
         span.style.filter = "blur(5px)";
         selection.getRangeAt(0).surroundContents(span);
