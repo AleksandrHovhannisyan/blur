5b4e252e02250a0b52355655a3883d510f376cba
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2024-10-04 18:58:27 -0500

    Use TS for vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    copyPublicDir: 'public',
+    copyPublicDir: true,
     outDir: 'dist',
     rollupOptions: {
       input: {
