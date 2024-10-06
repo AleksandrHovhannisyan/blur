8fa43871596fafb6e3f50452abd4507bb14ff918
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2024-10-06 13:06:58 -0500

    Use separate builds for chrome/firefox

---
+++ build.js
@@ -0,0 +1,34 @@
+import { build } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+async function customBuild(outDir, manifestSrc) {
+  await build({
+    build: {
+      outDir: outDir,
+      copyPublicDir: true,
+      rollupOptions: {
+        input: {
+          popup: "src/popup.html",
+          background: "src/background.ts",
+        },
+        output: {
+          entryFileNames: "src/[name].js",
+        },
+      },
+    },
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: manifestSrc,
+            dest: "", // Copy to the root of `outDir`
+            rename: "manifest.json",
+          },
+        ],
+      }),
+    ],
+  });
+}
+
+await customBuild("dist/chrome", "manifest-chrome.json");
+await customBuild("dist/firefox", "manifest-firefox.json");
