c87cde6abd10c71bacdef40627e5d697da580ed8
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2024-10-07 18:09:39 -0500

    Revert "Use chrome.runtime.onInstalled"
    This reverts commit 97338a68ed2a797e5e05c7c705c0f2dbeb8c2c02.

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,2 @@
-/** ID for the "Blur" context menu item. */
-export const BLUR_MENU_ITEM_ID = 'blur-text';
-
 /** CSS custom property used to blur elements on a page. */
 export const BLUR_INTENSITY_CUSTOM_PROPERTY = "__blur-intensity";
